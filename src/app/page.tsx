98473513f5f8c3d8f2a9b13ff5948d34a38d23e5
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-07-03 14:53:08 +0700

    feat: reorder imports and ensure SpecsComparison component is included in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import FeedingSchedule from "@/components/FeedingSchedule";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import FeedingSchedule from "@/components/FeedingSchedule";
+import SpecsComparison from "@/components/SpecsComparison";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Hero />
         <FeedingSchedule />
+        <SpecsComparison />
       </main>
     </>
   );
