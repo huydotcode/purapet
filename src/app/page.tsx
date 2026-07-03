f67ca457781cd7b9471c46bac595eb33fb6aa04f
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2026-07-03 14:29:58 +0700

    feat: add FeedingSchedule component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeedingSchedule from "@/components/FeedingSchedule";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <FeedingSchedule />
       </main>
     </>
   );
